5e710bc505be964027368188d6fb86124d2931c2
Rachit Jain <rachitjain9596@gmail.com>
2025-03-02 10:30:00 +0530

    youtube-clone shadcn-hsl bugfix add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
 	return (
 		<div>
-			<p className="font-bold text-rose-500">Hello World</p>
-			<Button>Click me</Button>
+			<Button variant="new">Clic</Button>
+			<Image src="/logo.svg" width={50} height={50} alt="logo" />
+			<p>YouTube</p>
 		</div>
 	);
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
 					"bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
-				new: "bg-purple-500 text-white",
+				new: "bg-destructive text-white",
 			},
 			size: {
 				default: "h-9 px-4 py-2",
