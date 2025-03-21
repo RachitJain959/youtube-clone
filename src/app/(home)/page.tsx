ce52bd8a98344b6c822dfd9072c4f703b1c2924e
Rachit Jain <rachitjain9596@gmail.com>
2025-03-21 10:30:00 +0530

    youtube-clone tRPC 7: (home)/client.tsx

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,15 @@
 import { trpc } from "@/trpc/server";
+import { PageClient } from "./client";
 
 export default async function Home() {
 	// const { data } = trpc.hello.useQuery({ text: "Ronny" }); client-side rendering
 
-	const data = await trpc.hello({ text: "Ronny" });
-	return <div>Client say: {data?.greeting}</div>;
+	// we are not using trpc on the server side to get the result in a const to use it
+	// we are using it to populate the data cache on the server side & then allowing the client comp to use the loaded data
+	void trpc.hello.prefetch({ text: "Ronny" });
+	return (
+		<div>
+			<PageClient />
+		</div>
+	);
 }
