7ad33a81d616f52d0d94de9e12d77c423d21a540
Rachit Jain <rachitjain9596@gmail.com>
2025-04-17 10:30:00 +0530

    youtube-clone vid-categs 5:prefetch categories

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
 
 	// we are not using trpc on the server side to get the result in a const to use it
 	// we are using it to populate the data cache on the server side & then allowing the client comp to use the loaded data
-	void trpc.hello.prefetch({ text: "Ronny" });
+	void trpc.categories.getMany.prefetch();
 	return (
 		<div>
 			<HydrateClient>
