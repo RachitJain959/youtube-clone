983bffcceb23531d687cc8c1c40caf6c9a97ac9e
Rachit Jain <rachitjain9596@gmail.com>
2025-05-08 10:30:00 +0530

    youtube-clone vid-categs 9:FilterCarousel2 fade

--- src/components/filter-carousel.tsx
+++ src/components/filter-carousel.tsx
@@ -4,11 +4,13 @@ import { cn } from "@/lib/utils"; // Dynamic tailwind class hack
 import { Badge } from "./ui/badge";
 import {
 	Carousel,
+	CarouselApi,
 	CarouselContent,
 	CarouselItem,
 	CarouselNext,
 	CarouselPrevious,
 } from "./ui/carousel";
+import { useEffect, useState } from "react";
 
 interface FilterCarouselProps {
 	value?: string | null;
@@ -26,15 +28,33 @@ export const FilterCarousel = ({
 	onSelect,
 	data,
 }: FilterCarouselProps) => {
+	// Selected Carousel Fade
+	const [api, setApi] = useState<CarouselApi>();
+	const [current, setCurrent] = useState(0); //starting position
+	const [count, setCount] = useState(0); //total items
+
+	useEffect(() => {
+		if (!api) return;
+
+		setCount(api.scrollSnapList().length);
+		setCurrent(api.selectedScrollSnap() + 1);
+
+		api.on("select", () => {
+			setCurrent(api.selectedScrollSnap() + 1);
+		});
+	}, [api]);
+
 	return (
 		<div className="relative w-full">
 			{/* LEft Fade */}
 			<div
 				className={cn(
 					"absolute top-0 bottom-0 left-12 w-12 z-10 bg-gradient-to-r from-white to-transparent pointer-events-none",
+					current === 1 && "hidden",
 				)}
 			/>
 			<Carousel
+				setApi={setApi}
 				opts={{
 					align: "start",
 					dragFree: true,
@@ -44,7 +64,7 @@ export const FilterCarousel = ({
 				<CarouselContent className="-ml-3">
 					<CarouselItem className="pl-3 basis-auto">
 						<Badge
-							variant={value === null ? "default" : "secondary"}
+							variant={!value ? "default" : "secondary"}
 							className="rounded-lg px-3 py-1 cursor-pointer whitespace-nowrap text-sm"
 						>
 							All
@@ -76,6 +96,7 @@ export const FilterCarousel = ({
 			<div
 				className={cn(
 					"absolute top-0 bottom-0 right-12 w-12 z-10 bg-gradient-to-l from-white to-transparent pointer-events-none",
+					current === count && "hidden",
 				)}
 			/>
 		</div>
