2a981c3b2e511596627667894017e5b1e02c7f9b
Rachit Jain <rachitjain9596@gmail.com>
2025-05-10 10:30:00 +0530

    youtube-clone vid-categs 9:FilterCarousel2 category searchParams

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -4,6 +4,7 @@ import { trpc } from "@/trpc/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
 import { FilterCarousel } from "@/components/filter-carousel";
+import { useRouter } from "next/navigation";
 
 interface CategoriesSectionProps {
 	categoryId?: string;
@@ -24,6 +25,7 @@ export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
 };
 
 const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
+	const router = useRouter();
 	const [categories] = trpc.categories.getMany.useSuspenseQuery();
 
 	const data = categories.map((category) => ({
@@ -31,10 +33,22 @@ const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
 		label: category.name,
 	}));
 
+	const onSelect = (value: string | null) => {
+		const url = new URL(window.location.href);
+
+		if (value) {
+			url.searchParams.set("categoryId", value);
+		} else {
+			url.searchParams.delete("categoryId");
+		}
+
+		router.push(url.toString());
+	};
+
 	return (
 		<div>
 			<FilterCarousel
-				onSelect={(x) => console.log(x)}
+				onSelect={onSelect}
 				value={categoryId}
 				data={data}
 			/>
