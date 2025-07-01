0725c2a80383bcd6c874662d2a8a6cd4f76e7e49
Rachit Jain <rachitjain9596@gmail.com>
2025-07-01 10:30:00 +0530

    youtube-clone mux:video-upload suc

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -23,10 +23,17 @@ const StudioUploadModal = () => {
 		<>
 			<ResponsiveModal
 				title="Upload a video"
-				open={!!create.data}
+				open={!!create.data?.url}
 				onOpenChange={() => create.reset()}
 			>
-				<StudioUploader />
+				{create.data?.url ? (
+					<StudioUploader
+						endpoint={create.data.url}
+						onSuccess={() => {}}
+					/>
+				) : (
+					<Loader2Icon />
+				)}
 			</ResponsiveModal>
 			<Button
 				variant="secondary"
