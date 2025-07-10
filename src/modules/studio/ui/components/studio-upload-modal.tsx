b30d55797bd9b31db277108b0f2a67712a008c21
Rachit Jain <rachitjain9596@gmail.com>
2025-07-10 10:30:00 +0530

    youtube-clone mux:resp-modal render

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ResponsiveModal } from "@/components/responsive-modal";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
@@ -18,18 +19,27 @@ const StudioUploadModal = () => {
 	});
 
 	return (
-		<Button
-			variant="secondary"
-			onClick={() => create.mutate()}
-			disabled={create.isPending}
-		>
-			{create.isPending ? (
-				<Loader2Icon className="animate-spin" />
-			) : (
-				<PlusIcon />
-			)}
-			Create
-		</Button>
+		<>
+			<ResponsiveModal
+				title="Upload a video"
+				open
+				onOpenChange={() => {}}
+			>
+				Uploader
+			</ResponsiveModal>
+			<Button
+				variant="secondary"
+				onClick={() => create.mutate()}
+				disabled={create.isPending}
+			>
+				{create.isPending ? (
+					<Loader2Icon className="animate-spin" />
+				) : (
+					<PlusIcon />
+				)}
+				Create
+			</Button>
+		</>
 	);
 };
 
