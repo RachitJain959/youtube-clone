cec5ce176e889e04d6147523669c3acef0f31a06
Rachit Jain <rachitjain9596@gmail.com>
2025-08-26 10:30:00 +0530

    youtube-clone mux-webhook:video.errored

--- src/app/api/videos/webhook/route.ts
+++ src/app/api/videos/webhook/route.ts
@@ -83,6 +83,20 @@ export const POST = async (request: Request) => {
 				.where(eq(videos.muxUploadId, data.upload_id));
 			break;
 		}
+
+		case "video.asset.errored": {
+			const data = payload.data as VideoAssetErroredWebhookEvent["data"];
+			if (!data.upload_id)
+				return new Response("Missing upload_id", { status: 400 });
+
+			await db
+				.update(videos)
+				.set({
+					muxStatus: data.status,
+				})
+				.where(eq(videos.muxUploadId, data.upload_id));
+			break;
+		}
 	}
 
 	return new Response("Webhook received", { status: 200 });
