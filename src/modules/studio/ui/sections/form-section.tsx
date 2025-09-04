d2acb11f8aa747f0f00c6e12b1131c2dcee58929
Rachit Jain <rachitjain9596@gmail.com>
2025-09-04 10:30:00 +0530

    youtube-clone video-form:from-section2

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -1,8 +1,17 @@
 "use client";
 
-import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+
+import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreVerticalIcon, TrashIcon } from "lucide-react";
 
 interface FormSectionProps {
 	videoId: string;
@@ -25,5 +34,32 @@ const FormSectionSkeleton = () => {
 const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
 	const [video] = trpc.studio.getOne.useSuspenseQuery({ id: videoId });
 
-	return <div>{JSON.stringify(video)}</div>;
+	return (
+		<div className="flex items-center justify-between mb-6 w-full">
+			<div>
+				<h1 className="text-2xl font-bold">Video Details</h1>
+				<p className="text-xs text-muted-foreground">
+					Manage your video details
+				</p>
+			</div>
+			<div className="flex items-center gap-x-2">
+				<Button type="submit" disabled={false}>
+					Save
+				</Button>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="ghost" size="icon">
+							<MoreVerticalIcon />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end">
+						<DropdownMenuItem>
+							<TrashIcon className="size-4 mr-2" />
+							Delete
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+		</div>
+	);
 };
