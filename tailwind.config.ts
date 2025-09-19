8ff51dcea7a7987676195bb4fd2de647bbef5ac3
Rachit Jain <rachitjain9596@gmail.com>
2025-09-19 10:30:00 +0530

    youtube-clone ai-thum:uploadthing.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
+import { withUt } from "uploadthing/tw";
+
 import type { Config } from "tailwindcss";
 
-export default {
+export default withUt({
 	darkMode: ["class"],
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -91,4 +93,4 @@ export default {
 		},
 	},
 	plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+}) satisfies Config;
